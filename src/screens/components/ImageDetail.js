d887e3922be7edd7b333b4ebefc3495f542f094f
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-15 13:06:42 +0100

    Excercise: Added score

--- src/screens/components/ImageDetail.js
+++ src/screens/components/ImageDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
 
-const ImageDetail = props => {
-    console.log(props)
+const ImageDetail = ({title, imageSource, score}) => {
     return (
         <View>
-            <Image source={props.imageSource}/>
-            <Text>{props.title}</Text>
+            <Image source={imageSource}/>
+            <Text>{title}</Text>
+            <Text>Image score - {score}</Text>
         </View>
     );
 }
