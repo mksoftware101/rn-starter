ae009b506e1ac619bc7777ba8004149879ceeaab
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-22 15:29:53 +0100

    Playing with parent/child layout props

--- src/screens/layouts/BoxLayout.js
+++ src/screens/layouts/BoxLayout.js
@@ -4,7 +4,9 @@ import {Text, View, StyleSheet} from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Abc</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     );
 }
@@ -12,12 +14,21 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        height: 200,
     },
-    textStyle: {
-        borderWidth: 1,
+    textOneStyle: {
+        borderWidth: 3,
         borderColor: 'red',
-        margin: 20
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        top: 10 // layout elements, then shift 10px down
+    },
+    textThreeStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
     }
 })
 
